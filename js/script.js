fba42e5691c69ce177667e3482016ebf0c572bf4
Denis <denisbormusov@gmail.com>
2023-03-10 11:01:03 +0500

    init example

---
+++ js/script.js
@@ -0,0 +1,58 @@
+let pageSlider = new Swiper('.page', {
+
+
+    wrapperClass: "page__wrapper",
+
+    slideClass: "page__screen",
+
+
+
+    direction: 'vertical',
+
+    slidesPerView: 1,
+
+
+    keyboard: {
+        enabled: true,
+
+        onlyInViewport: true,
+
+        pageUpDown: true,
+    },
+
+
+    mousewheel: {
+        sensitivity: 1,
+
+    },
+
+
+    watchOverflow: true,
+
+    parallax: true,
+
+    speed: 800,
+
+    observer: true,
+
+    observeParents: true,
+
+    observeSlideChildren: true,
+
+
+    /* pagination: {
+        el: 'page__pagination',
+        type: 'bullets',
+        clickable: true,
+        bulletClass: 'page__bullet',
+        bulletActiveClass: "page__bullet_active",
+    }, */
+
+  /*   scrollbar: {
+        el: 'page__scroll',
+        dragClass: 'page__drag-scroll',
+
+        draggable: true,
+    }, */
+
+})
